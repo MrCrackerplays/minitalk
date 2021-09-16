a110f149beeee12df9f02e25cecf490a2bb6cb0f
pdruart <pdruart@student.codam.nl>
2021-09-16 11:29:06 +0200

    spacing in header

--- headers/minitalk.h
+++ headers/minitalk.h
@@ -6,12 +6,13 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/07 15:49:56 by pdruart       #+#    #+#                 */
-/*   Updated: 2021/09/09 16:23:32 by pdruart       ########   odam.nl         */
+/*   Updated: 2021/09/16 11:28:32 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+
 typedef struct s_signal_data
 {
 	int		pid;
